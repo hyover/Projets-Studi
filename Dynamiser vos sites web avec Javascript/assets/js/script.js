6fd73ec54c0f5f0d2625e68bac50d8123c723c1a
Valentin FONTAINE <vf.hyover@gmail.com>
2022-04-07 09:54:28 +0200

    Dynamiser vos sites web avec Javascript - Back End - New Game - Completed

--- Dynamiser vos sites web avec Javascript/assets/js/script.js
+++ Dynamiser vos sites web avec Javascript/assets/js/script.js
@@ -1,5 +1,22 @@
 /* NEW GAME */
 
+function newGame() {
+    
+    //Select name players
+    let player1 = prompt("Change Player1 name");
+    let player2 = prompt("Change player2 name");
+
+    document.getElementById("player1").innerHTML = player1;
+    document.getElementById("player2").innerHTML = player2;
+
+    // Reset stats
+    document.getElementById("global-player1").innerHTML = "0";
+    document.getElementById("global-player2").innerHTML = "0";
+    document.getElementById("round-player1").innerHTML = "0";
+    document.getElementById("round-player2").innerHTML = "0";
+}
+
+
 /* ROLL THE DICE */
 
 let images = ["src/dice-01.svg",
@@ -20,7 +37,6 @@ function roll(){
             die.classList.remove("shake");
         });
         let dieOneValue = Math.floor(Math.random()*6);
-        console.log(dieOneValue);
         document.querySelector("#die").setAttribute("src", images[dieOneValue]);
         document.querySelector("#round-player1").innerHTML = (dieOneValue + 1);
     },
