cc0d0290d31ff15cd0fce94f0bb99592d8cabcb9
Valentin FONTAINE <vf.hyover@gmail.com>
2022-04-08 07:51:00 +0200

    Dynamiser vos sites web avec Javascript - Back End - Completed - Game Finish

--- Dynamiser vos sites web avec Javascript/assets/js/script.js
+++ Dynamiser vos sites web avec Javascript/assets/js/script.js
@@ -1,5 +1,14 @@
 var globalScore, roundScore, activePlayer, gamePlaying
 
+let images = ["src/dice-01.svg",
+"src/dice-02.svg",
+"src/dice-03.svg",
+"src/dice-04.svg",
+"src/dice-05.svg",
+"src/dice-06.svg"]
+
+let dice = document.querySelectorAll("img")
+
 /* NEW GAME */
 
 function newGame() {
@@ -10,37 +19,35 @@ function newGame() {
     gamePlaying = true
     
     //Select name players
-    let player1 = prompt("Nom du joueur 1");
-    let player2 = prompt("Nom du joueur 2");
+    let player1 = prompt("Nom du joueur 1")
+    let player2 = prompt("Nom du joueur 2")
 
-    document.getElementById("player-0").innerHTML = player1;
-    document.getElementById("player-1").innerHTML = player2;
+    document.getElementById("player-0").innerHTML = player1
+    document.getElementById("player-1").innerHTML = player2
 
     // Reset stats
-    document.getElementById("global-0").innerHTML = "0";
-    document.getElementById("global-1").innerHTML = "0";
-    document.getElementById("round-0").innerHTML = "0";
-    document.getElementById("round-1").innerHTML = "0";
+    document.getElementById("global-0").innerHTML = "0"
+    document.getElementById("global-1").innerHTML = "0"
+    document.getElementById("round-0").innerHTML = "0"
+    document.getElementById("round-1").innerHTML = "0"
+    
+    dice.forEach(function(die){
+        die.classList.remove("shake-winner")
+    })
+
+    // Add player active
+    document.getElementById("active-0").classList.add("player-active")
 }
 
 
 /* ROLL THE DICE */
-
-let images = ["src/dice-01.svg",
-"src/dice-02.svg",
-"src/dice-03.svg",
-"src/dice-04.svg",
-"src/dice-05.svg",
-"src/dice-06.svg"];
-
-let dice = document.querySelectorAll("img")
 
 function roll(){
     if (gamePlaying) {
         // 1. Shake Dice
         dice.forEach(function(die){
             die.classList.add("shake")
-        });
+        })
         
         setTimeout(function(){
             dice.forEach(function(die){
@@ -51,17 +58,17 @@ function roll(){
         )
 
         // 2. Display the Result
-        let dieValue = Math.floor(Math.random()*6);
+        let dieValue = Math.floor(Math.random()*6)
         document.querySelector("#die").setAttribute("src", images[dieValue])
 
         //3. Update the round score IF the rolled number was NOT a 1
         if (dieValue !== 0) {
             //Add score
-            roundScore += dieValue + 1;
+            roundScore += dieValue + 1
             document.querySelector('#round-' + activePlayer).textContent = roundScore
         } else {
             //Next player
-            nextPlayer();
+            nextPlayer()
         }
     }
     
@@ -72,21 +79,21 @@ function roll(){
 function hold() {
     if (gamePlaying) {
         // Add ROUND score in current box to GLOBAL score
-        globalScore[activePlayer] += roundScore;
+        globalScore[activePlayer] += roundScore
 
         // Update the scores
-        document.getElementById('global-' + activePlayer).textContent = globalScore[activePlayer];
+        document.getElementById('global-' + activePlayer).textContent = globalScore[activePlayer]
 
         // Check if player won the game
         if (globalScore[activePlayer] >= 100) {
-            document.querySelector('#player-' + activePlayer).textContent = 'GAGNANT!';
+            document.querySelector('#player-' + activePlayer).textContent = 'GAGNANT!'
             dice.forEach(function(die){
-                die.classList.add("shake")
-            });
-            gamePlaying = false;
+                die.classList.add("shake-winner")
+            })
+            gamePlaying = false
         } else {
             //Next player
-            nextPlayer();
+            nextPlayer()
         }
     }
 }
@@ -95,9 +102,13 @@ function hold() {
 
 function nextPlayer() {
     //Next player
-    activePlayer === 1 ? activePlayer = 0 : activePlayer = 1;
-    roundScore = 0;
+    activePlayer === 1 ? activePlayer = 0 : activePlayer = 1
+    roundScore = 0
 
-    document.getElementById('round-0').textContent = '0';
+    document.getElementById('round-0').textContent = '0'
     document.getElementById('round-1').textContent = '0'
+
+    // Change player active
+    document.getElementById("active-0").classList.toggle("player-active")
+    document.getElementById("active-1").classList.toggle("player-active")
 }
